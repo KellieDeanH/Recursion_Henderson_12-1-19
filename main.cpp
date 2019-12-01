5ba8ce5ae3aebcb540b045c0079a700bfa36dd92
Kellie H <kellie.henderson@my.tccd.edu>
2019-12-01 23:42:02 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//Kellie Henderson, Dr.T, COSC 1437, 12/1/19
+// A program to allow users to find a sum using recursion
+
+#include <iostream>
+#include <fstream>
+void countDown (int num);
+
+
+void countDown(int num)
+
+{ 	
+int num = 0;
+
+	 if (num == 0)
+
+		   cout << "Blastoff!"<< endl;
+
+		else
+
+		{ 
+
+	    cout << num << "...\n";
+
+		   // recursive
+
+	 }                    // call
+
+}
+
+
+
+int main() 
+{
+
+ countDown(num-1);
+
+ return 0;
+}
